21120bf4f41bcc020ed8b6367045bbaa47f2a05f
Pieter van der Westhuizen <pieter@csoft.co.za>
2016-09-05 08:28:16 +0200

    Chapter 6 - Add SalesPerSalesPerson

--- Chapters/Chapter6/src/Chapter6/Controllers/HomeController.cs
+++ Chapters/Chapter6/src/Chapter6/Controllers/HomeController.cs
@@ -12,5 +12,18 @@ namespace Chapter6.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public JsonResult SalesPerSalesPerson()
+        {
+            List<object> data = new List<object>();
+            data.Add(new string[] { "Product", "Sales" });
+            data.Add(new object[] { "Robert King", 530 });
+            data.Add(new object[] { "Nancy Davolio", 1012 });
+            data.Add(new object[] { "Laura Callahan", 810 });
+            data.Add(new object[] { "Janet Leverling", 738 });
+            return Json(data);
+        }
+
     }
 }
